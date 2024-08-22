1c94c89964aef81f5575384f3b1ecd88bbb30a43
jatin-rajput563 <jkt3298@gmail.com>
2024-08-22 13:57:08 +0530

    change in html css and js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,91 @@
 $('.one-time').slick({
     dots: true,
-    arrows: false,
     infinite: true,
+    autoPlay: true,
     speed: 300,
     slidesToShow: 1.7,
-    adaptiveHeight: true
+    slidesToScroll: 1,
+    centerMode: false,
+    autoplayspeed: 2000,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+
+    responsive: [
+
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 786,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+
 });
 
 $('.multiple-items').slick({
     infinite: true,
-    dots:true,
+    autoPlay: true,
+    dots: true,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 2,
+    // autoplayspeed: 2000,
+    // autoplay: true,
+    centerpadding: '60px',
+
+    responsive: [
+
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 786,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
 });
 
 $('.show-three').slick({
     dots: true,
-    arrows:true,
-    slidesToShow: 3,
+    infinite: true,
+    centerMode: true,
+    centerpadding: '60px',
+    autoPlay: true,
+    autoplayspeed: 3000,
+    slidesToShow: 2,
     slidesToScroll: 1,
-    // autoplay: true,
-    autoplaySpeed: 3000,
+    adaptiveHeight: true,
     prevArrow: $('.prev'),
     nextArrow: $('.next'),
+    responsive: [
+
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 786,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
 });
